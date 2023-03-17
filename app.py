7e0d015162c8715a30b79d21d86db6fb6679f310
Nirs123 <nirs123@protonmail.com>
2023-03-17 01:04:46 +0100

    Update 17/03/2023
    -Added map and pie viz
    -Added geojson files
    -Created requirements.txt
    -Finished about page

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+# Import libraries
 from flask import Flask, request, render_template,jsonify
-import data
-import visualisation as viz
 import io
 from PIL import Image
 import base64
 import os
+import random
+import string
+import time
+# Import files
+import data
+import visualisation as viz
 
 app = Flask(__name__)
 
@@ -35,30 +40,107 @@ def background_process_bar():
     gender = request.args.get('gender',"",str)
     tmpLoc = request.args.get('location',"",str)
 
-    locationErrors = ["--Lieu--","--Régions--","--Académies--"]
-    if year == "--Année--" or classe == "--Classe--" or gender == "--Genre--" or tmpLoc in locationErrors:
+    if "Choisissez" in [year[:10], classe[:10], gender[:10], tmpLoc[:10]]:
+        print("test")
         return jsonify(res=False)
-    
 
     location = tmpLoc.split(",")[0]
     locationName = None
     if tmpLoc != "fr":
         locationName = tmpLoc.split(",")[1]
 
+    characters = string.ascii_letters + string.digits
+    filename = ''.join(random.choice(characters) for i in range(20))
+
+    TIME_start = time.time()
     dataResult = data.speCount(classe+".csv",year,gender,location,locationName)
+    TIME_datascrap = time.time() - TIME_start
+    TIME_start = time.time()
     plt = viz.barPlot(dataResult,year,classe,gender,location,locationName)
     img_buf = io.BytesIO()
     plt.savefig(img_buf, format='png')
     im = Image.open(img_buf)
-    im.save("test.png")
+    im.save(filename+".png")
     tmpData = {}
-    with open('test.png', mode='rb') as file:
+    with open(filename+".png", mode='rb') as file:
         img = file.read()
     tmpData['img'] = base64.encodebytes(img).decode('utf-8')
+    os.remove(filename+".png")
+    TIME_image = time.time() - TIME_start
 
-    os.remove('test.png')
+    return jsonify(res=True,image=tmpData['img'],scrap_time=TIME_datascrap,image_time=TIME_image)
 
-    return jsonify(res=True,image=tmpData['img'])
+@app.route('/background_process_pie')
+def background_process_pie():
+    year = request.args.get('year',"",str)
+    classe = request.args.get('class',"",str)
+    tmpLoc = request.args.get('location',"",str)
+    spe = request.args.get('spe',"",str)
+    gender = "All"
+
+    if "Choisissez" in [year[:10], classe[:10], spe[:10], tmpLoc[:10]]:
+        return jsonify(res=False)
+
+    location = tmpLoc.split(",")[0]
+    locationName = None
+    if tmpLoc != "fr":
+        locationName = tmpLoc.split(",")[1]
+
+    characters = string.ascii_letters + string.digits
+    filename = ''.join(random.choice(characters) for i in range(20))
+
+    TIME_start = time.time()
+    dataResult = data.speCount(classe+".csv",year,gender,location,locationName)
+    TIME_datascrap = time.time() - TIME_start
+    TIME_start = time.time()
+    plt = viz.piePlot(dataResult,year,classe,location,locationName,spe)
+    img_buf = io.BytesIO()
+    plt.savefig(img_buf, format='png')
+    im = Image.open(img_buf)
+    im.save(filename+".png")
+    tmpData = {}
+    with open(filename+".png", mode='rb') as file:
+        img = file.read()
+    tmpData['img'] = base64.encodebytes(img).decode('utf-8')
+    os.remove(filename+".png")
+    TIME_image = time.time() - TIME_start
+
+    return jsonify(res=True,image=tmpData['img'],scrap_time=TIME_datascrap,image_time=TIME_image)
+
+# todo
+@app.route('/background_process_map')
+def background_process_map():
+    year = request.args.get('year',"",str)
+    classe = request.args.get('class',"",str)
+    spe = request.args.get('spe',"",str)
+    loc = request.args.get('location',"",str)
+
+    if "Choisissez" in [year[:10], classe[:10], spe[:10], loc[:10]]:
+        return jsonify(res=False)
+
+    characters = string.ascii_letters + string.digits
+    filename = ''.join(random.choice(characters) for i in range(20))
+
+    TIME_start = time.time()
+    if loc == "dep":
+        dataResult = data.speCountDep(classe+".csv",year,spe)
+    elif loc == "region":
+        dataResult = data.speCountRegion(classe+".csv",year,spe)
+    TIME_datascrap = time.time() - TIME_start
+    TIME_start = time.time()
+    plt = viz.mapPlot(dataResult,year,classe,loc,spe)
+    img_buf = io.BytesIO()
+    plt.savefig(img_buf, format='png')
+    im = Image.open(img_buf)
+    im.save(filename+".png")
+    tmpData = {}
+    with open(filename+".png", mode='rb') as file:
+        img = file.read()
+    tmpData['img'] = base64.encodebytes(img).decode('utf-8')
+    os.remove(filename+".png")
+    TIME_image = time.time() - TIME_start
+
+    return jsonify(res=True,image=tmpData['img'],scrap_time=TIME_datascrap,image_time=TIME_image)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/script/script.js
+++ static/script/script.js
@@ -25,11 +25,99 @@ $(function()
                 myImg.removeAttribute("hidden");
                 myImg.src = "data:image/png;base64, "+data.image;
 
+                console.log("SCRAP TIME: "+data.scrap_time);
+                console.log("IMAGE TIME: "+data.image_time);
+
                 document.getElementById('barbtn').disabled = false;
             }
             else
             {
+                alert("Une erreur est parvenue lors de la création de votre graphique");
+                document.getElementById('barbtn').disabled = false;
+            }
+        })
+        return false;
+    });
+});
+
+// PIE
+$(function() 
+{
+    $('#piebtn').on('click', function() 
+    {
+        document.getElementById('piebtn').disabled = true;
+
+        let myYear = document.getElementById("year").options[document.getElementById("year").selectedIndex].value;
+        let myClass = document.getElementById("class").options[document.getElementById("class").selectedIndex].value;
+        let myLocation = document.getElementById("location").options[document.getElementById("location").selectedIndex].value;
+        let mySpe = document.getElementById("spe").options[document.getElementById("spe").selectedIndex].value;
+        $.getJSON('/background_process_pie',
+        {
+            year:myYear,
+            class:myClass,
+            location:myLocation,
+            spe:mySpe
+        },function(data)
+        {
+            if (data.res == true)
+            {
+                let myRow = document.getElementById('resRow');
+                myRow.removeAttribute("hidden");
+                let myImg = document.getElementById('resImage');
+                myImg.removeAttribute("hidden");
+                myImg.src = "data:image/png;base64, "+data.image;
+
+                console.log("SCRAP TIME: "+data.scrap_time);
+                console.log("IMAGE TIME: "+data.image_time);
+
+                document.getElementById('piebtn').disabled = false;
+            }
+            else
+            {
                 alert("Une erreur est parvenue lors de la création de votre graphique")
+                document.getElementById('piebtn').disabled = false;
+            }
+        })
+        return false;
+    });
+});
+
+// MAP
+$(function() 
+{
+    $('#mapbtn').on('click', function() 
+    {
+        document.getElementById('mapbtn').disabled = true;
+
+        let myYear = document.getElementById("year").options[document.getElementById("year").selectedIndex].value;
+        let myClass = document.getElementById("class").options[document.getElementById("class").selectedIndex].value;
+        let mySpe = document.getElementById("spe").options[document.getElementById("spe").selectedIndex].value;
+        let myLocation = document.getElementById("location").options[document.getElementById("location").selectedIndex].value;
+        $.getJSON('/background_process_map',
+        {
+            year:myYear,
+            class:myClass,
+            spe:mySpe,
+            location:myLocation
+        },function(data)
+        {
+            if (data.res == true)
+            {
+                let myRow = document.getElementById('resRow');
+                myRow.removeAttribute("hidden");
+                let myImg = document.getElementById('resImage');
+                myImg.removeAttribute("hidden");
+                myImg.src = "data:image/png;base64, "+data.image;
+
+                console.log("SCRAP TIME: "+data.scrap_time);
+                console.log("IMAGE TIME: "+data.image_time);
+
+                document.getElementById('mapbtn').disabled = false;
+            }
+            else
+            {
+                alert("Une erreur est parvenue lors de la création de votre graphique")
+                document.getElementById('mapbtn').disabled = false;
             }
         })
         return false;
